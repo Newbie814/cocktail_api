8008dc0b941b533533c2e605a7d6f1f744bf5f55
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-07 22:54:20 -0400

    initial project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import pages
+import Home from './pages/Home';
+import About from './pages/About';
+import SingleCocktail from './pages/SingleCocktail';
+import Error from './pages/Error';
+// import components
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className='App'>
-      <h2>Cocktail API</h2>
+    <div>
+      <h2>app component</h2>
     </div>
   );
 }
