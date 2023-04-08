e748bc5d51f446bebff62e3b9d8d75b6c3256098
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-08 16:33:41 -0400

    setup home cocktail page and search functionality

--- src/components/Cocktail.jsx
+++ src/components/Cocktail.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useGlobalContext } from '../context';
 
-const Cocktail = () => {
+// id: idDrink,
+//           name: strDrink,
+//           image: strDrinkThumb,
+//           info: strAlcoholic,
+//           glass: strGlass,
+
+const Cocktail = ({ id, name, image, info, glass }) => {
   return (
-    <div>
-      <h2>cocktail component</h2>
-    </div>
-  )
-}
+    <article className='cocktail'>
+      <div className='img-container'>
+        <img src={image} alt={name} />
+      </div>
+      <div className='cocktail-footer'>
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className='btn btn-primary btn-details'>
+          details
+        </Link>
+      </div>
+    </article>
+  );
+};
 
-export default Cocktail
+export default Cocktail;
